8004b5fc53b69ce8080ddf730e5871f62208e867
Jonas Acres <acresjonas@gmail.com>
2017-09-03 15:05:21 -0700

    use ngrep instead

--- pi/weathersnarf.rb
+++ pi/weathersnarf.rb
@@ -1,75 +1,68 @@
-#!/usr/bin/env ruby
-
-# listens for tcpdump -X output capturing smartHUB data, fed over a TCP socket
-#
-# feed me data using a sniff like this from the router
-# tcpdump -X host SMARTHUB_IP_ADDRESS and port 80 | nc LOCAL_IP_ADDRESS 10100
-# please don't forward me packets that aren't AcuRite smartHUB data, i'm not really tested for that
+#!/usr/bin/ruby
 
 require 'socket'
-require 'cgi'
 require 'sqlite3'
 require 'json'
 
-$http_keystore_url = "http://10.0.1.125:11000" # comment out to disable http-keystore use
+$http_keystore_url = "http://owl:11000" # comment out to disable http-keystore use
+accumulator = ""
 
-def parse_line(line)
-  comps = line.strip.split(/\s+/)
-
-  if comps.first =~ /^\d{2}:\d{2}:\d{2}.\d{6}/ then
-    {
-      type: :start,
-      time:comps[0],
-      src:comps[2],
-      dest:comps[4].gsub(":", ""),
-      flags:comps[6][1..-3].split(""),
-      seq_start:comps[8].split(":").first.to_i,
-      seq_stop:comps[8].split(":").last.to_i,
-      length:comps[14].to_i
-    }
-  else
-    {
-      type: :data,
-      data: comps[1..-2].join("").gsub(/../) { |pair| pair.hex.chr }
-    }
-  end
+def logged_keys
+  [ :sensor, :windspeedmph, :winddir, :rainin, :dailyrainin, :humidity, :tempf, :dewptf, :baromin ]
 end
 
 def log(msg)
   puts "#{Time.now.strftime("%Y-%m-%d %H:%M:%S -- ")}" + msg
 end
 
-def monitor(client)
-  context = { }
+def parse_query(raw)
+  args = {}
+  @last ||= {}
+  @last[raw["sensor"]] ||= {}
+  last_record = @last[raw["sensor"]]
 
-  while text = client.gets do
-    text.split("\n").each do |line|
-      parsed = parse_line(line)
-      if parsed[:type] == :start then
-        context = { data:"", src:parsed[:src], dest:parsed[:dest], flags:parsed[:flags], length:parsed[:length] }
-      else
-        context[:data] += parsed[:data]
-        if context[:flags].include?("P") && context[:data].length == 256*context[:data][6].ord+context[:data][7].ord + 4 then
-          link_offset = 4
-          tcp_offset = link_offset + 4*(context[:data][4].ord & 0x0F)
-          data_offset = tcp_offset + 4*((context[:data][tcp_offset+12].ord & 0xF0) >> 4)
+  logged_keys.each do |key|
+    k = key.to_s
+    last_record[k] = raw[k] if raw[k]
+    args[k] = last_record[k]
+  end
 
-          yield ({ src:context[:src], dest:context[:dest], data:context[:data][data_offset..-1], flags:context[:flags] })
-        elsif context[:flags].include?("F") then
-          yield ({ src:context[:src], dest:context[:dest], data:"", flags:context[:flags] })
-        end
-      end
+  args[:time_since] = Time.now.to_i - (last_record[:last_seen] || 0)
+  args[:different] = args != last_record[:last_args]
+  last_record[:last_seen] = Time.now.to_i
+  last_record[:last_args] = args
+
+  args
+end
+
+def send_http_keystore(args)
+  return unless $http_keystore_url
+
+  Thread.new do
+    url = File.join($http_keystore_url, "sensor-#{args["sensor"]}")
+    IO.popen "curl -X POST -d @- #{url} 1>/dev/null 2>&1", 'r+' do |io|
+      io.puts args.to_json
     end
   end
 end
 
-def conn_hash(src, dest)
-  src + ">" + dest
-end
+def record_entry(query)
+  args = parse_query(query)
+  return unless args[:time_since] >= 60 || args[:different] # only log if it has been at least 60 seconds, or if the data has changed
+  logged_keys.each { |key| return unless args[key.to_s] }
 
-def process_msg(msg)
-  m = msg.match(/GET ([^\x00\s]+[\x00\s])/)
-  yield m[1] if m
+  args.merge!({ "query" => "", "time_inserted_epoch" => Time.now.to_i, "time_inserted_str" => Time.now.strftime("%Y-%m-%d %H:%M:%S %z") })
+  insert_keys = args.keys.select { |k| k.is_a?(String) }
+  send_http_keystore(args)
+
+  db = open_db("weather.db")
+  log "Inserting keys: #{insert_keys.map { |k| "#{k} = #{args[k.to_s]}" }.join(", ")}"
+  log "INSERT INTO weather (#{insert_keys.join(", ")}) values (#{insert_keys.map { "?" }.join(", ")})"
+  log insert_keys.map { |k| args[k.to_s] }.count.to_s
+
+  db.execute "INSERT INTO weather (#{insert_keys.join(", ")}) values (#{insert_keys.map { "?" }.join(", ")})", insert_keys.map { |k| args[k.to_s] }
+
+  log "Sensor:#{args["sensor"]} Temp:#{args["tempf"]}F Rain:#{args["dailyrainin"]}\" Wind:#{args["windspeedmph"]}mph Dir:#{args["winddir"]}deg Baro:#{args["baromin"]} Humid:#{args["humidity"]}% Dew:#{args["dewptf"]}F"
 end
 
 def open_db(path)
@@ -100,63 +93,29 @@ def open_db(path)
     db.execute <<-SQL
       create index time_index on weather (time_inserted_epoch)
     SQL
+
+    log "Created database #{weather.db}"
   end
 
   db
 end
 
-def logged_keys
-  [ :sensor, :windspeedmph, :winddir, :rainin, :dailyrainin, :humidity, :tempf, :dewptf, :baromin ]
-end
+def monitor(client)
+  accumulator = ""
 
-def parse_query(query)
-  raw = CGI::parse(query.split("?")[1..-1].join("?").strip)
-  args = {}
-  
-  @last ||= {}
-  @last[raw["sensor"].first] ||= {}
-  last_record = @last[raw["sensor"].first]
+  while text = client.gets do
+    text.split("\n").each do |line|
+      next if line.start_with?("T ")
+      accumulator += line
 
-  logged_keys.each do |key|
-    k = key.to_s
-    if raw[k] && raw[k].first then
-      last_record[k] = raw[k].first
-    end
-    args[k] = last_record[k]
-  end
-
-  args[:time_since] = Time.now.to_i - (last_record[:last_seen] || 0)
-  args[:different] = args != last_record[:last_args]
-  last_record[:last_seen] = Time.now.to_i
-  last_record[:last_args] = args
-
-  args
-end
-
-def send_http_keystore(args)
-  return unless $http_keystore_url
-
-  Thread.new do
-    url = File.join($http_keystore_url, "sensor-#{args["sensor"]}")
-    IO.popen "curl -X POST -d @- #{url} 1>/dev/null 2>&1", 'r+' do |io|
-      io.puts args.to_json
+      if m = accumulator.match(/GET \/weatherstation\/updateweatherstation\?([^ ]+) HTTP/) then
+        h = {}
+        m[1].split("&").map { |pair| pair.split("=") }.each { |p| h[p[0]] = p[1] }
+        accumulator = ""
+        yield(h)
+      end
     end
   end
-end
-
-def record_entry(query)
-  args = parse_query(query)
-  return unless args[:time_since] >= 60 || args[:different] # only log if it has been at least 60 seconds, or if the data has changed
-
-  args.merge!({ "query" => query, "time_inserted_epoch" => Time.now.to_i, "time_inserted_str" => Time.now.strftime("%Y-%m-%d %H:%M:%S %z") })
-  insert_keys = args.keys.select { |k| k.is_a?(String) }
-  send_http_keystore(args)
-
-  db = open_db("weather.db")
-
-  db.execute "INSERT INTO weather (#{insert_keys.join(", ")}) values (#{insert_keys.map { "?" }.join(", ")})", insert_keys.map { |k| args[k.to_s] }
-
-  log "Sensor:#{args["sensor"]} Temp:#{args["tempf"]}F Rain:#{args["dailyrainin"]}\" Wind:#{args["windspeedmph"]}mph Dir:#{args["winddir"]}deg Baro:#{args["baromin"]} Humid:#{args["humidity"]}% Dew:#{args["dewptf"]}F"
 end
 
 def run!(port)
@@ -171,15 +130,8 @@ def run!(port)
     Thread.new do
       begin
         connections = {}
-        monitor(client) do |packet|
-          h = conn_hash(packet[:src], packet[:dest])
-          connections[h] ||= { data:"" }
-          connections[h][:data] += packet[:data]
-
-          if packet[:flags].include?("F") then
-            process_msg(connections[h][:data]) { |q| record_entry(q) }
-            connections.delete(h)
-          end
+        monitor(client) do |request|
+          record_entry(request)
         end
 
         log "Closed connection to client: #{client.peeraddr[2]}"
